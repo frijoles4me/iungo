e790101e4164d836d464ac47e93485c53684f5cb
alvaro cortes <frijoles4me@gmail.com>
2018-05-01 23:29:27 -0700

    added comment, likes with a delet opition

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ require("./config/passport")(passport);
 // Use Routes
 app.use("/api/users", users);
 app.use("/api/profile", profile);
-app.use("/api/post", posts);
+app.use("/api/posts", posts);
 
 const port = process.env.PORT || 5000;
 
